e9f07fe98601cde3f02e66e7eec9b12bc0c3ff93
Sathish R <sathishjj8@gmail.com>
2020-08-02 23:03:46 +0530

    Updates

---
+++ precache-manifest.d7ac7c66e5c74374e8d732383711817b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc26ae6ce223d47896511016e505df00",
+    "url": "/MyReact/index.html"
+  },
+  {
+    "revision": "90810eb495f934b38f51",
+    "url": "/MyReact/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "070e81324fdd28ff8f32",
+    "url": "/MyReact/static/css/main.02050e33.chunk.css"
+  },
+  {
+    "revision": "90810eb495f934b38f51",
+    "url": "/MyReact/static/js/2.3eee1732.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MyReact/static/js/2.3eee1732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "070e81324fdd28ff8f32",
+    "url": "/MyReact/static/js/main.8e802179.chunk.js"
+  },
+  {
+    "revision": "2cec5ac71bcd58d4f353",
+    "url": "/MyReact/static/js/runtime-main.57b35da5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MyReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
